25db6689821ebb7d3c41351b1316f7bd1e5b0a2b
root <798587094@qq.com>
2019-02-25 00:26:07 +0800

    liubin first web

---
+++ test1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title> liubin site</title>
+</head>
+<body>
+<p>
+<center>
+<?php
+echo "hello this is a my first web\n";
+phpinfo();
+?>
+</center>
+</p>
+</body>
+</html>
